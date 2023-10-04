bc45112c3d8e319d9d1762e047795c37c939dbb1
kamil.kurek <kamil.kurek@sts.pl>
2023-10-04 23:24:15 +0200

    interfaces, shape assignment

---
+++ ShapeAssignment/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+
+func main() {
+	square := square{sideLength: 10}
+	triangle := triangle{base: 10, height: 10}
+	printArea(square)
+	printArea(triangle)
+
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return t.height * (t.base / 2)
+}
